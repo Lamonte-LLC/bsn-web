38adb205250fa0b97e0f33ad1e22ca08763d31fc
Lamonte <hola@lamonte.studio>
2026-02-26 15:15:43 -0400

    fix: Wrong param for fgmplayerleaderscard

--- src/stats/widgets/season/player/FGMPlayerLeadersCard.tsx
+++ src/stats/widgets/season/player/FGMPlayerLeadersCard.tsx
@@ -15,7 +15,7 @@ type PlayerLeadersResponse = {
 const fetchTopPlayerLeaders = async (): Promise<TopPlayerLeaderStatsType[]> => {
   const { data, error } = await getClient().query<PlayerLeadersResponse>({
     query: SEASON_TOP_PLAYER_LEADER_STATS_BY_CATEGORY,
-    variables: { statsCode: 'FG_AVG', first: ITEMS_PER_PAGE },
+    variables: { statsCode: 'FIELD_GOALS_AVG', first: ITEMS_PER_PAGE },
   });
 
   if (error) {
